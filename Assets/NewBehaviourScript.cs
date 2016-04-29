bc1f0f7c7c50f62b73bd356dcb590dc866d62c66
Sirslask <sirslask@hotmail.com>
2016-04-29 22:59:45 +0200

    A

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -14,24 +14,35 @@ public class NewBehaviourScript : MonoBehaviour {
 
     public float RotationSpeed = 200.0f;
 
+    
+
     void UpdateFunction()
     {
+
+
+            
+       }
+
+    // Update is called once per frame
+    void Update () {
+
         Quaternion AddRot = Quaternion.identity;
         float roll = 0;
         float pitch = 0;
         float yaw = 0;
-        roll = Input.GetAxis("Roll") * (Time.deltaTime * RotationSpeed);
-        pitch = Input.GetAxis("Pitch") * (Time.deltaTime * RotationSpeed);
-        yaw = Input.GetAxis("Yaw") * (Time.deltaTime * RotationSpeed);
+// roll = Input.GetAxis("Roll") * (Time.deltaTime * RotationSpeed);
+// pitch = Input.GetAxis("Pitch") * (Time.deltaTime * RotationSpeed);
+        //yaw = Input.GetAxis("Yaw") * (Time.deltaTime * RotationSpeed);
         AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
         GetComponent<Rigidbody>().rotation *= AddRot;
         Vector3 AddPos = Vector3.forward;
         AddPos = playerShip.GetComponent<Rigidbody>().rotation * AddPos;
         GetComponent<Rigidbody>().velocity = AddPos * (Time.deltaTime * AmbientSpeed);
-    }
 
-    // Update is called once per frame
-    void Update () {
-	
-	}
+
+        if (Input.GetKey("up"))
+        {
+            AmbientSpeed = 500;
+        }
+    }
 }
