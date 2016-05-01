4acdc3c392505cc5808b09ca011790b55104691d
Sirslask <sirslask@hotmail.com>
2016-05-01 17:13:19 +0200

    sofa king awesome

--- Assets/ShipRotation.cs
+++ Assets/ShipRotation.cs
@@ -4,28 +4,12 @@ using System.Collections;
 public class ShipRotation : MonoBehaviour {
 
 
-    public GameObject playerShip;
-    public Rigidbody shipbody;
-    public float AmbientSpeed = 100.0f;
-
-    public float RotationSpeed = 200.0f;
-
-    public float movementSpeed = 0;
-
-    float roll = 0;
-    float pitch = 0;
-    float yaw = 0;
-
-    public int fuel = 100;
-
-
     // Use this for initialization
     void Start () {
 
-        shipbody = playerShip.GetComponent<Rigidbody>();
     }
 
-
+    
     void UpdateFunction()
     {
             
@@ -36,21 +20,6 @@ public class ShipRotation : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        Quaternion AddRot = Quaternion.identity;
-
-
-        AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
-        GetComponent<Rigidbody>().rotation *= AddRot;
-        Vector3 AddPos = Vector3.forward;
-        AddPos = playerShip.GetComponent<Rigidbody>().rotation * AddPos;
-        GetComponent<Rigidbody>().velocity = AddPos * (Time.deltaTime * AmbientSpeed);
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            print(fuel);
-            boost();
-        }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -96,14 +65,14 @@ public class ShipRotation : MonoBehaviour {
 
         bool pastBottomScreenEdge = Camera.main.WorldToScreenPoint(transform.position).y > Screen.height;
 
-        transform.Translate(0, pastBottomScreenEdge ? 0 : .2f, 0);
+        transform.Translate(0, pastBottomScreenEdge ? 0 : .5f, 0);
     }
 
 
     void flyUp()
         {
         bool pastTopScreenEdge = Camera.main.WorldToScreenPoint(transform.position).y <= 0;
-        transform.Translate(0, pastTopScreenEdge ? 0 : -.2f, 0);
+        transform.Translate(0, pastTopScreenEdge ? 0 : -.5f, 0);
     }
 
 
@@ -111,27 +80,14 @@ public class ShipRotation : MonoBehaviour {
         {
         bool pastLeftScreenEdge = Camera.main.WorldToScreenPoint(transform.position).x <= 0;
 
-        transform.Translate(pastLeftScreenEdge ? 0 : -.2f, 0, 0);
+        transform.Translate(pastLeftScreenEdge ? 0 : -.5f, 0, 0);
         }
 
     void flyRigth()
         {
         bool pastRightScreenEdge = Camera.main.WorldToScreenPoint(transform.position).x >= Screen.width;
 
-        transform.Translate(pastRightScreenEdge ? 0 : .2F, 0, 0);
-        }
-
-    void boost()
-        {
-            if(fuel > 0)
-            {
-                GetComponentInParent<Cameracontrol>().AmbientSpeed = 800;
-                fuel -= 1;
-                print(fuel);
-                print("boost");
-
-            }
-            else GetComponentInParent<Cameracontrol>().AmbientSpeed = 0;
+        transform.Translate(pastRightScreenEdge ? 0 : .5F, 0, 0);
         }
 
     }
--- Assets/shipcontrol.cs
+++ Assets/shipcontrol.cs
@@ -1,15 +1,110 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class shipcontrol : MonoBehaviour {
+public class shipcontrol : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform Target;
+    public float RotationSpeed;
+
+    //values for internal use
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    void UpdateFunction()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //find the vector pointing from our position to the target
+        _direction = (Target.position - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        _lookRotation = Quaternion.LookRotation(_direction);
+
+        //rotate us over time according to speed until we are in the required rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            flyUp();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            flyDown();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            flyLeft();
+        }
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            flyRigth();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    // Ship flight controll
+
+    void flyDown()
+    {
+
+        bool pastBottomScreenEdge = Camera.main.WorldToScreenPoint(transform.position).y > Screen.height;
+
+        transform.Translate(0, pastBottomScreenEdge ? 0 : .2f, 0);
+    }
+
+
+    void flyUp()
+    {
+        bool pastTopScreenEdge = Camera.main.WorldToScreenPoint(transform.position).y <= 0;
+        transform.Translate(0, pastTopScreenEdge ? 0 : -.2f, 0);
+    }
+
+
+    void flyLeft()
+    {
+        bool pastLeftScreenEdge = Camera.main.WorldToScreenPoint(transform.position).x <= 0;
+
+        transform.Translate(pastLeftScreenEdge ? 0 : -.2f, 0, 0);
+    }
+
+    void flyRigth()
+    {
+        bool pastRightScreenEdge = Camera.main.WorldToScreenPoint(transform.position).x >= Screen.width;
+
+        transform.Translate(pastRightScreenEdge ? 0 : .2F, 0, 0);
+    }
+
 }
